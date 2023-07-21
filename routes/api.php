22bceb8067b6b6442c5610d62cbfa5d1ffabef47
Diego Cardoso <diegocar448@hotmail.com>
2023-07-21 17:25:43 -0300

    EnviandoImagemParaOServidor

--- routes/api.php
+++ routes/api.php
@@ -116,8 +116,32 @@ Route::middleware('auth:api')->put('/perfil', function (Request $request) {
         $user->email = $data['email'];
     }
 
+    if (isset($data['imagem'])) {
+        $time = time();
+        $diretorioPai = 'http://127.0.0.1:8000/perfils';
+        $diretorioImagem = $diretorioPai.DIRECTORY_SEPARATOR.'perfil_id'.$user->id;
+        $ext = substr($data['imagem'], 11, strpos($data['imagem'], ';') -11);
+        $urlImagem = $diretorioImagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
+
+        $file = str_replace('data:image/'.$ext.';base64,', '', $data['imagem']);
+        $file = base64_decode($file);
+
+        if (!file_exists($diretorioPai)) {
+            mkdir($diretorioPai, 0700);
+        }
+        if (!file_exists($diretorioImagem)) {
+            mkdir($diretorioImagem, 0700);
+        }
+
+        file_put_contents($urlImagem, $file);
+
+        $user->imagem = $urlImagem;
+    }
+
     
     $user->save();
+
+    $user->token = asset($user->imagem);
     $user->token = $user->createToken($user->email)->accessToken;
 
     return $user;
